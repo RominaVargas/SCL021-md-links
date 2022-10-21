a1512763e900a5b2bf3a012e134f05f9c4b26193
RominaVargas <rominita.malfoy@gmail.com>
2022-10-20 23:24:10 -0300

    actualizacion readme

--- codigo.js
+++ codigo.js
@@ -6,8 +6,10 @@ let userRoute = process.argv[2];
 let formatOK = ".md"; 
 let colors = require('colors');
 let linkFinder = /(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/gi;
+//expresion regular necesaria para detectar links.
 const https = require('https');
 
+//PASO 1
 //volver la ruta a relativa para partir
 let absolutRoute = () => {
     if(path.isAbsolute(userRoute) === false){
@@ -17,7 +19,6 @@ let absolutRoute = () => {
 };
 console.log("Pasando ruta relativa a ruta absoluta: ".bgCyan + path.resolve(userRoute));
     
-/* let formatValidator = (userRoute, formatOK) => {*/
     let resultAbsolutRoute = absolutRoute(path); 
     let toArr = resultAbsolutRoute.split(",");
     console.log(toArr);
@@ -29,13 +30,13 @@ console.log("Pasando ruta relativa a ruta absoluta: ".bgCyan + path.resolve(user
         } else {
             console.log('El formato del archivo no es válido!'.bgYellow);
             };
-
+//PASO 2
 function readLinks (){
     return new Promise ((resolve, reject) => {
        let count = [];      
               fs.readFile(userRoute, 'utf-8', (err, data) => {
          if(err) {
-      //console.log('error: ', err);
+      //data es el contenido de la ruta. Links, texto;
       reject (err);
        } else {
       count = data.match(linkFinder);
@@ -55,20 +56,31 @@ const uniqueLinks = (infoLinks) => {
   }) 
   return console.log('El total de links únicos encontrados es: '.bgMagenta, unique);
 };
+//PASO 3
 const validLinks = (count) => {
      return count.map(link => {
         return new Promise((resolve, reject) => {
             https.get(link, res => {
                 if(res.statusCode === 200) {
-                    resolve({count: process.argv[2], url: link, code: res.statusCode, message: "OK"})
+                    resolve({
+                      //count: process.argv[2], 
+                      url: link, 
+                      code: res.statusCode, 
+                      message: "El link se encuentra OK"
+                    })
                 } else {
-                  reject({count: process.argv[2], url: link, code: res.statusCode, message: "FAIL"})
+                  reject({
+                    count: process.argv[2], 
+                    url: link, 
+                    code: res.statusCode, 
+                    message: "El link no se encuentra"
+                  })
                 }
               })
         })
     })
 };
-
+//PASO 4
 readLinks()
   .then((result) => {
     console.log(result);
